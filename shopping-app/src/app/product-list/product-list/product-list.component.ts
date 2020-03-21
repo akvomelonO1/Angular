dbfadfb1b13255abe07574f476c80ba8f76fd7dd
Eloy González-Posada Gil <akvomelono@MBP-de-Eloy.home>
2020-03-21 20:33:05 +0100

    Adding commits in few lines

--- shopping-app/src/app/product-list/product-list/product-list.component.ts
+++ shopping-app/src/app/product-list/product-list/product-list.component.ts
@@ -6,16 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  constructor() {}
+  products = [
+    {
+      name: 'Iphone 3',
+      description:
+        
+        ' El iPhone 3GS marcó un cambio importante en la potencia del teléfono, al incluir un procesador ARM Cortex A8, con lo que aumentó su eficacia a los 600 MHz y a una memoria RAM de 256 MB. Con este terminal, Apple prácticamente dobló las posibilidades del modelo anterior, evolución que luego iría aplicando en los nuevos lanzamientos.'
+    },
+    {
+      name: 'Iphone 4',
+      description:
+        'Como ha especificado Apple, la versión del teléfono en color blanco daba problemas con el funcionamiento normal del teléfono, lo que retrasó su salida al mercado. En España, el iPhone 4 blanco se lanzó a través de la tienda Apple el 28 de abril de 2011.1​'
+    },
+    {
+      name: 'Iphone 5',
+      description: '' /* En caso de no haber descripción el ngIf del div no muestra esta sección. */
+        
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   // products = products;
 
   share() {
     window.alert('The product has been shared!');
   }
-
 }
